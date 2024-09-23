5c34b25a6cab57e829f0373d0281a336d61ec311
Alfred Thomas <alfredthomas900@gmail.com>
2024-09-23 10:27:14 +0530

    Employee Service - Changed the URI of get all employees API
    Signed-off-by: Alfred Thomas <alfredthomas900@gmail.com>

--- employee-service/src/main/java/com/alfredthomas/employeeservice/constants/EmployeeURIConstants.java
+++ employee-service/src/main/java/com/alfredthomas/employeeservice/constants/EmployeeURIConstants.java
@@ -3,7 +3,7 @@ package com.alfredthomas.employeeservice.constants;
 public final class EmployeeURIConstants {
   public static final String EMPLOYEE_BASE_URI = "/employee/service";
   public static final String CREATE_EMPLOYEE_URI = "/create";
-    public static final String GET_ALL_EMPLOYEES_URI = "/all";
+  public static final String GET_ALL_EMPLOYEES_URI = "/employees/all";
 
-    private EmployeeURIConstants() {}
+  private EmployeeURIConstants() {}
 }
