22775a714317d527d5eb1f82dc8c3fe939ec2896
Alfred Thomas <alfredthomas900@gmail.com>
2024-09-23 07:50:19 +0530

    Employee Service - Added CrossOrigin annotation to resolve CORS issues while calling the API from the UI side
    Signed-off-by: Alfred Thomas <alfredthomas900@gmail.com>

--- employee-service/src/main/java/com/alfredthomas/employeeservice/controller/EmployeeController.java
+++ employee-service/src/main/java/com/alfredthomas/employeeservice/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import com.alfredthomas.employeeservice.model.request.EmployeeRequest;
 import com.alfredthomas.employeeservice.model.response.EmployeeResponse;
 import com.alfredthomas.employeeservice.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(EmployeeURIConstants.EMPLOYEE_BASE_URI)
 @RequiredArgsConstructor
+@CrossOrigin(originPatterns = "http://localhost:3000")
 public class EmployeeController {
   private final EmployeeService employeeService;
 
